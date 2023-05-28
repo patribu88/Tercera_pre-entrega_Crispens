bf415759ef7b05b77083b7352c4a5efad55bbf66
Patribu88 <patribucrispens@gmail.com>
2023-05-28 17:35:25 -0300

    Create models en home. Animales y usuarios

--- project/apps/home/models.py
+++ project/apps/home/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class Usuario(models.Model):
+    nombre = models.CharField(max_length=30)
+    apellido = models.CharField(max_length=30)
+    edad = models.IntegerField()
+    email = models.EmailField()
+
+    def __str__(self) -> str:
+        return self.apellido
+    
+
+class Animales(models.Model):
+    Animal = models.CharField(max_length=30)
+    def __str__(self) -> str:
+        return self.Animal
