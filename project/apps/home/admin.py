7c18ef45ec8ad06c9da069ba45ef0f6426e13517
Patribu88 <patribucrispens@gmail.com>
2023-05-28 18:00:04 -0300

    Crear herencia de plantilas y modelos de orgs y regiones

--- project/apps/home/admin.py
+++ project/apps/home/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 
-from .models import Usuario, Animales
+from .models import Usuario, Animales, Region, Organizacion
 
 admin.site.register(Usuario)
 
 admin.site.register(Animales)
+
+admin.site.register(Region)
+
+admin.site.register(Organizacion)
--- project/apps/home/models.py
+++ project/apps/home/models.py
@@ -16,3 +16,17 @@ class Animales(models.Model):
     Animal = models.CharField(max_length=30)
     def __str__(self) -> str:
         return self.Animal
+
+
+class Region(models.Model):
+    nombre = models.CharField(max_length=30)
+    def __str__(self) -> str:
+        return self.nombre
+    
+
+class Organizacion(models.Model):
+    nombre = models.CharField(max_length=30)
+    email = models.EmailField()
+    region_id = models.ForeignKey(Region, on_delete=models.SET_NULL, null=True)
+    def __str__(self) -> str:
+        return self.nombre
