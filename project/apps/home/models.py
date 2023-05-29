5b0e010817dd2a0e90cbefbc77644e227832fd8e
Patribu88 <patribucrispens@gmail.com>
2023-05-29 12:25:22 -0300

    Update comentarios en las clases

--- project/apps/home/models.py
+++ project/apps/home/models.py
@@ -3,6 +3,9 @@ from django.db import models
 # Create your models here.
 
 class Usuario(models.Model):
+    """
+    Usuario de la aplicación. Puede ser una persona potencial para transitar animales o representantes de la organizaciones protectoras de animales aptos para cargar animales para transitar.
+    """
     nombre = models.CharField(max_length=30)
     apellido = models.CharField(max_length=30)
     edad = models.IntegerField()
@@ -13,18 +16,27 @@ class Usuario(models.Model):
     
 
 class Animales(models.Model):
+    """
+    Tipo de animal disponible para transitar.
+    """
     Animal = models.CharField(max_length=30)
     def __str__(self) -> str:
         return self.Animal
 
 
 class Region(models.Model):
+    """
+    Área de la cobertura de la organización protectora de animales.
+    """
     nombre = models.CharField(max_length=30)
     def __str__(self) -> str:
         return self.nombre
     
 
 class Organizacion(models.Model):
+    """
+    Organizaciones protectoras de animales que poseen la responsabilidad del animal en tránsito encargadas de gestionar la adopción.
+    """
     nombre = models.CharField(max_length=30)
     email = models.EmailField()
     region_id = models.ForeignKey(Region, on_delete=models.SET_NULL, null=True)
@@ -32,11 +44,17 @@ class Organizacion(models.Model):
         return self.nombre
 
 class Sexo(models.Model):
+    """
+    Sexo de los animales disponibles para transitar.
+    """
     sexo = models.CharField(max_length=30)
     def __str__(self) -> str:
         return self.sexo
     
 class Transito(models.Model):
+    """
+    Animal disponible para transitar hasta su adopción definitiva.
+    """
     nombre = models.CharField(max_length=30)
     edad = models.IntegerField()
     organizacion_id = models.ForeignKey(Organizacion, on_delete=models.SET_NULL, null=True)
